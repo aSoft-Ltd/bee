c71983941dbd92fac3b65141433a097e9ba34aed
George Sechu <admin@mitikaz.com>
2023-03-28 11:12:13 +0300

    Before pushing

--- core/src/commonMain/kotlin/bee/DeliverSummary.kt
+++ core/src/commonMain/kotlin/bee/DeliverSummary.kt
@@ -4,7 +4,7 @@ package bee
 import kotlin.js.JsExport
 import kotlinx.serialization.Serializable
 @Serializable
-data class DeliverSummary(
-    val entries: List<Delivery>,
-    val status: DeliveryStatus
+class DeliverSummary(
+//    val entries: List<Delivery>,
+//    val status: DeliveryStatus
 )
