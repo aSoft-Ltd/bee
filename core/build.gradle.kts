c5e6f718ca2b438d56fda472297155c93aed2612
andylamax <andylamax@programmer.net>
2024-02-19 12:12:08 +0300

    Reintroduced wasm back

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -44,10 +44,10 @@ rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
     args.add("--ignore-engines")
 }
 
-//tasks.named("wasmJsTestTestDevelopmentExecutableCompileSync").configure {
-//    mustRunAfter(tasks.named("jsBrowserTest"))
-//    mustRunAfter(tasks.named("jsNodeTest"))
-//}
+tasks.named("wasmJsTestTestDevelopmentExecutableCompileSync").configure {
+    mustRunAfter(tasks.named("jsBrowserTest"))
+    mustRunAfter(tasks.named("jsNodeTest"))
+}
 
 aSoftOSSLibrary(
     version = libs.versions.asoft.get(),
